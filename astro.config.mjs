ff86153faa59fc8b2a9d68880da0dfb3fd1cb42b
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2025-05-19 16:28:48 -0500

    Configuración en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'middleware' }),
+  experimental: {
+    session: true,
+  },
   integrations: [
     tailwind(),
     react()
